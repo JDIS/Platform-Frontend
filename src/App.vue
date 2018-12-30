8ca7cf5f1d8959a4228c230704bf332d1d8834dc
Émilio Gonzalez <egg997@gmail.com>
2018-12-30 12:19:54 -0800

    Add bootstrap 4 and make a nicer navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <ul id="nav" class="nav justify-content-end">
+      <li class="nav-item">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+    <div id="main-container" class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -15,15 +21,23 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  #main-container {
+    max-width: 1600px;
+  }
 }
 #nav {
-  padding: 30px;
+  background-color: #2d9d5f;
+  padding: 15px;
   a {
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+  .nav-item {
+    margin: 0 10px;
   }
 }
 </style>
